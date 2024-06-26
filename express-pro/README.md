cd7c368b8a30abfe2028a04e31e9f264d6f047e7
AbdulsalamAmtech <abdulsalamamtech@gmail.com>
2024-06-26 12:04:13 +0100

    Add express pro directory

---
+++ express-pro/README.md
@@ -0,0 +1,3 @@
+# Nodejs Express Api
+
+## The best way to structure your express api directories
---
+++ express-pro/src/configs/app.config.js
@@ -0,0 +1,3 @@
+const dbUrlTest = "mongodb+srv://amtech:<password>@cluster0.7td15ke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const dbUrl = "mongodb+srv://amtech:pass.mongo.atlas@cluster0.7td15ke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
